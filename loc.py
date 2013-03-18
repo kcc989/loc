1c2cfe9212eb14fc282bf2db0e4fcf26a26dab22
kcc989 <chcollin@hamilton.edu>
2013-03-17 21:27:24 -0300

    Update loc.py

--- loc.py
+++ loc.py
@@ -1,35 +1,46 @@
-import os
+import os, time, shutil, locator
 from os import listdir
 from os.path import isfile, join
 
-def ensure(f,y):
-	d = os.path.dirname(y)
-	;print y
-	if not os.path.isdir(y):
-		os.makedirs(y)
-	
-		
+def ensure(f):
+	if not os.path.isdir(f):
+		os.makedirs(f)
 
+def member(element, lst):
+	for i in lst:
+		if i == element:
+			return True
+	return False
+	
 def main():
-	years = [10,11,12,13]
-	loc = os.path.dirname(os.path.abspath(__file__))
-	for year in years:
-		yearDir = (loc + "\\" + str(year))
-		print yearDir
-		ensure(yearDir,year)
+	loc = locator.module_path()
 	onlyfiles = [ f for f in listdir(loc) if isfile(join(loc,f)) ]
-	modDate = []
-	locPath = os.listdir(loc)
 	realNames = []
 	for fname in onlyfiles:
 		realNames.append(loc + "\\" + fname)
+	fileYear = []
 	for i in realNames:
 		if os.path.isfile(i):
-			z = os.path.getmtime(i)
-			modDate.append(z)
-		
+			fileYear.append(time.ctime(os.path.getmtime(i))[20:24])
 	i = 0
-	while i < len(modDate):
-		#print locPath[i] + ": " + str(modDate[i])
+	yearPath = []
+	years = []
+	for i in fileYear:
+		if member(i, years) == False:
+			years.append(i)
+	for year in years:
+		yearDir = (loc + "\\" + str(year))
+		ensure(yearDir)
+		yearPath.append(yearDir)
+	i = 0
+	while i < len(realNames):
+		j = 0
+		destination = ""
+		while j < len(yearPath):
+			if fileYear[i] == years[j]:
+				destination = yearPath[j]	
+			j += 1
+		if destination != "":
+				shutil.move(realNames[i],destination)
 		i += 1
 main()
